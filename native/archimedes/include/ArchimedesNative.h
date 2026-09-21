add5546e53b8adcdb5fd77099f30fd12da95638b
Oak Grove Slim <54104732+ATphobia22@users.noreply.github.com>
2026-09-21 09:30:26 -0500

    fix(native): correct CRS units and add bounded Saint-Venant solver

--- native/archimedes/include/ArchimedesNative.h
+++ native/archimedes/include/ArchimedesNative.h
@@ -15,8 +15,8 @@ extern "C" {
 #endif
 
 typedef struct ArchimedesSiteInput {
-    double x_epsg2966_m;
-    double y_epsg2966_m;
+    double x_epsg2966_ftus;
+    double y_epsg2966_ftus;
     double bfe_ft;
     double lag_ft;
     double stage_delta_ft;
@@ -26,18 +26,36 @@ typedef struct ArchimedesSiteMetrics {
     double bfe_ft;
     double lag_ft;
     double surface_water_elevation_ft;
+    int inundation_active;
 } ArchimedesSiteMetrics;
+
+typedef struct ArchimedesSaintVenantConfig {
+    double cell_width_ft;
+    double bed_slope;
+    double mannings_n;
+    double gravity_ft_s2;
+    double cfl;
+} ArchimedesSaintVenantConfig;
 
 typedef enum ArchimedesStatus {
     ARCHIMEDES_OK = 0,
     ARCHIMEDES_INVALID_ARGUMENT = 1,
     ARCHIMEDES_NON_FINITE_INPUT = 2,
-    ARCHIMEDES_INVALID_GEOMETRY = 3
+    ARCHIMEDES_INVALID_GEOMETRY = 3,
+    ARCHIMEDES_UNSTABLE_TIMESTEP = 4,
+    ARCHIMEDES_DRY_STATE = 5
 } ArchimedesStatus;
 
 ARCHIMEDES_API ArchimedesStatus ArchimedesEvaluateGeodeticInvariants(
     const ArchimedesSiteInput* input,
     ArchimedesSiteMetrics* output);
+
+ARCHIMEDES_API ArchimedesStatus ArchimedesSolveSaintVenant1D(
+    const ArchimedesSaintVenantConfig* config,
+    double* depth_ft,
+    double* velocity_fps,
+    int cell_count,
+    double dt_seconds);
 
 ARCHIMEDES_API const char* ArchimedesStatusName(ArchimedesStatus status);
 
