a3597fd4f922e2fc12f60042e47563685a406fc9
Oak Grove Slim <54104732+ATphobia22@users.noreply.github.com>
2026-09-22 23:58:22 -0500

    feat: enable browser runtime performance telemetry

--- tsm-console/src/main.tsx
+++ tsm-console/src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router';
 import { router } from './lib/router';
+import { installRuntimePerformanceTelemetry } from './lib/runtime-performance';
+
+installRuntimePerformanceTelemetry(router);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
