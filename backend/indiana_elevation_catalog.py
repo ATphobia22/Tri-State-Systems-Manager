59549aee32a2354e392153ac84a449b142d0e699
Oak Grove Slim <54104732+ATphobia22@users.noreply.github.com>
2026-09-22 23:36:55 -0500

    feat(geospatial): record verified Posey 2020 LiDAR coverage metadata

--- backend/indiana_elevation_catalog.py
+++ backend/indiana_elevation_catalog.py
@@ -13,6 +13,8 @@ INDIANA_ELEVATION_CATALOG_URL = "https://elevation.gio.in.gov/"
 INDIANA_ELEVATION_S3_BROWSER_URL = "https://giselevationingov.s3.amazonaws.com/index.html"
 INDIANA_ELEVATION_BUCKET_ARN = "arn:aws:s3:::giselevationingov"
 INDIANA_ELEVATION_REGION = "us-east-2"
+INDIANA_POSEY_2020_COVERAGE_URL = "https://gis.iu.edu/s/isdp/page/20162020elevation"
+INDIANA_POSEY_2020_EXAMPLE_ITEM_URL = "https://gis.iu.edu/s/isdp/item/86452"
 INDIANA_LIDAR_VIEWER_URL = (
     "https://indianamap-inmap.hub.arcgis.com/maps/"
     "ff98e3834d464619bd5c8974b0038a13/about"
@@ -37,6 +39,11 @@ def source_manifest() -> dict[str, str]:
         "bucket_arn": INDIANA_ELEVATION_BUCKET_ARN,
         "region": INDIANA_ELEVATION_REGION,
         "viewer_url": INDIANA_LIDAR_VIEWER_URL,
+        "posey_collection_year": "2020",
+        "posey_collection_format": "LAS",
+        "posey_collection_units": "feet",
+        "posey_coverage_index_url": INDIANA_POSEY_2020_COVERAGE_URL,
+        "posey_example_quad_url": INDIANA_POSEY_2020_EXAMPLE_ITEM_URL,
         "license": "CC0",
         "data_classes": "LAS LiDAR; elevation archive",
         "professional_certification_authority": "NONE",
